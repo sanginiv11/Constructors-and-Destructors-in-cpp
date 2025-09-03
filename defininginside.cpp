5651517b7ff0dc5b3fec6797bc882ecaa7ecf785
Sangini Verma <sanginiv11@gmail.com>
2025-09-04 04:11:27 +0530

    Add files via upload

---
+++ defininginside.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+
+class student {
+    int rollno;
+    char name[50];
+    double fee;
+
+public:
+    student() {
+        cout << "Enter the rollno: ";
+        cin >> rollno;
+        cout << "Enter the name: ";
+        cin >> name;
+        cout << "Enter the Fee: ";
+        cin >> fee;
+    }
+
+    void display() {
+        cout << "Roll No: " << rollno << endl;
+        cout << "Name: " << name << endl;
+        cout << "Fee: " << fee << endl;
+    }
+};
+
+int main() {
+    student s1;
+    s1.display();
+    return 0;
+}
+// Output
+// Enter the rollno: 93
+// Enter the name: Sangini
+// Enter the Fee: 400000
+// Roll No: 93
+// Name: Sangini
+// Fee: 400000
+// Note: The output will vary based on user input.
